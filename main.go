ef0af0d50c8c45ec080ab76e9637e3b121634727
Andrey <folganoid@yahoo.com>
2019-08-19 22:12:36 +0300

    (fnc) next figure, speed

--- main.go
+++ main.go
@@ -20,8 +20,10 @@ const (
 const unitSize = 20
 const fieldSizeX = 260
 const fieldSizeY = 600
+
 var x = 5
-var y = 15
+var y = 10
+var speed = 500
 var rotate = 0
 var figureNow figure
 
@@ -34,7 +36,7 @@ type figureOnePos struct {
 	positions [4] string
 }
 
-var field = [fieldSizeX/unitSize][fieldSizeY/unitSize]bool{}
+var field = [fieldSizeY/unitSize][fieldSizeX/unitSize]int{}
 
 
 func checkPosDown(xCrd int, yCrd int, fig figure) bool {
@@ -47,7 +49,9 @@ func checkPosDown(xCrd int, yCrd int, fig figure) bool {
 		tmpY, _ := strconv.Atoi(cellCoords[1])
 		tmpY += yCrd
 
-		if tmpY + 1 >= fieldSizeY/unitSize || field[tmpX][tmpY] == true {
+		if tmpY + 1 >= fieldSizeY/unitSize || field[tmpY+1][tmpX] > 0 {
+			addFigureToField(xCrd, yCrd, fig, rotate)
+			speed = 500
 			return false
 		}
 	}
@@ -56,9 +60,44 @@ func checkPosDown(xCrd int, yCrd int, fig figure) bool {
 
 }
 
+/**
+* add figure to field
+ */
+func addFigureToField(xCrd int, yCrd int, fig figure, rotate int) {
+	for _, cell := range fig.coords[rotate].positions {
+
+		cellCoords := strings.Split(cell, ".")
+		tmpX, _ := strconv.Atoi(cellCoords[0])
+		tmpX += xCrd
+		tmpY, _ := strconv.Atoi(cellCoords[1])
+		tmpY += yCrd
+		fmt.Println(tmpY, tmpX)
+		field[tmpY][tmpX] = 1
+	}
+
+	fmt.Println(figure{})
+}
+
+func drawField(cr *cairo.Context) *cairo.Context {
+
+
+	for i:=0; i<len(field);i++{
+		for z:=0; z<len(field[i]);z++{
+			if field[i][z] > 0 {
+				cr.Rectangle(float64(z*unitSize), float64(i*unitSize), float64(unitSize), float64(unitSize))
+			}
+		}
+	}
+
+	cr.Fill()
+	return cr
+}
+
 func main() {
 
 	gtk.Init(nil)
+
+	field[5][3] = 1
 
 	// gui boilerplate
 	win, _ := gtk.WindowNew(gtk.WINDOW_TOPLEVEL)
@@ -81,17 +120,14 @@ func main() {
 		},
 		KEY_RIGHT: func() { x++ },
 		KEY_DOWN:  func() {
-			if rotate <= 0 {
-				rotate = 3
-			} else {
-				rotate--
-			}
+			speed = 25
 		},
 	}
 
 	// Event handlers
 	da.Connect("draw", func(da *gtk.DrawingArea, cr *cairo.Context) {
 		cr = fig2(cr)
+		cr = drawField(cr)
 	})
 
 	win.Connect("key-press-event", func(win *gtk.Window, ev *gdk.Event) {
@@ -111,10 +147,11 @@ func main() {
 
 			if  !checkPosDown(x,y,figureNow) {
 				fmt.Println("+++++++++++++")
-				break
+				x = 5
+				y = 10
 			}
 
-			time.Sleep(time.Second)
+			time.Sleep(time.Duration(speed) * time.Millisecond)
 			y++
 			win.QueueDraw()
 
